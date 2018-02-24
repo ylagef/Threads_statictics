3f81edbd6d8150af4e0084a2d5774f32a940315c
Yeray Lage <ylagef@gmail.com>
2018-02-24 19:21:33 +0100

    Done.

--- src/Main.java
+++ src/Main.java
@@ -5,43 +5,37 @@ import java.util.List;
 import static java.lang.Math.*;
 
 public class Main {
-    public static HashMap<Integer, Output> outputs = new HashMap<>();
-    public static int numThreads = 0;
+    public static HashMap<Integer, Output> outputs = new HashMap<>(); //Global HashMap with the Output objects.
+    public static int numThreads;
 
     public static void main(String[] args) {
         long globalInit = System.currentTimeMillis();
         numThreads = Integer.parseInt(args[0]);
 
-        List<Thread> threadList = new ArrayList<>(numThreads);
+        List<Thread> threadList = new ArrayList<>(numThreads); //ArrayList for the created Threads.
 
+        //Create and start Threads. As much as selected by the argument value.
         for (int i = 0; i < numThreads; ++i) {
             threadList.add(i, new Thread(new OneThread(), Integer.toString(i)));
-            threadList.get(i).start();
             Main.outputs.put(i, new Output(System.currentTimeMillis()));
+            threadList.get(i).start();
         }
 
-        //For each thread, wait until is ended.
-        //Then, this foreach ends when every thread is ended.
-        for (Thread t : threadList) {
+        for (Thread t : threadList) { //For each thread, wait until is ended.
             try {
-                t.join();
-                //join() is a function that waits until Thread t is finished.
-                //Then, this loop will wait until each thread has ended.
-
-                long end = System.currentTimeMillis();
-                Main.outputs.get(Integer.parseInt(t.getName())).setEnded(end);
-
+                t.join(); //join() is a function that waits until Thread t is finished.
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+        //Then, this foreach ends when every thread is ended.
 
-        //Printing each Thread timing for evaluation
+        //Printing each Thread timing for evaluation.
         for (int i = 0; i < numThreads; i++) {
             System.out.println("Thread " + i + " - " + outputs.get(i).toString());
         }
 
-        //Prints number of threads and total time needed (for statistics)
+        //Prints number of threads and total time needed (for statistics).
         System.out.println("\n" + args[0] + " threads, total time: " + (System.currentTimeMillis() - globalInit));
 
         //Print final message
@@ -49,23 +43,25 @@ public class Main {
     }
 }
 
-//Custom Thread class implementing Runnable
+//Custom Thread class implementing Runnable.
 class OneThread implements Runnable {
-
-    public void run() { //Code going to be executed by the thread.
-        //Store values into the object
-        long init = System.currentTimeMillis();
+    //Code going to be executed by the thread.
+    public void run() {
+        long init = System.currentTimeMillis(); //Start of execution.
 
         for (int i = 0; i < 1000000 / Main.numThreads; i++) {
             double d = tan(atan(tan(atan(tan(atan(tan(atan(tan(atan(123456789.123456789))))))))));
             cbrt(d);
         }
 
+        //Store the init and end values into the object.
         Main.outputs.get(Integer.parseInt(Thread.currentThread().getName())).setStarted(init);
+        long end = System.currentTimeMillis(); //End of execution.
+        Main.outputs.get(Integer.parseInt(Thread.currentThread().getName())).setEnded(end);
     }
 }
 
-//Class used for storing each Thread timing
+//Class used for storing each Thread timing.
 class Output {
     private long queued, started, ended;
 
